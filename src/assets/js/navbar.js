eed8c06426be4480ca8625d7c8461fc509b2bc22
Jenn <jjalava456@hotmail.com>
2019-08-29 13:06:03 -0500

    agregando animaciones a la pagina principal

--- src/assets/js/navbar.js
+++ src/assets/js/navbar.js
@@ -3,77 +3,35 @@
 // Theme module
 //
 
-'use strict';
 
-(function() {
-  
-  //
-  // Variables
-  //
-
-  var navbar = document.querySelector('.navbar');
-  var isLight = false;
-  var isTogglable = navbar ? navbar.classList.contains('navbar-togglable') : false;
-
-
-  //
-  // Functions
-  //
-
-  function makeNavbarLight() {
-    if (!isLight && isTogglable) {
-      navbar.classList.remove('navbar-dark');
-      navbar.classList.add('navbar-light');
-      navbar.classList.add('bg-white');
-      navbar.classList.add('border-bottom');
-
-      isLight = true;
+$(document).ready(function() {
+  // Custom 
+  var stickyToggle = function(sticky, stickyWrapper, scrollElement) {
+    var stickyHeight = sticky.outerHeight();
+    var stickyTop = stickyWrapper.offset().top;
+    if (scrollElement.scrollTop() >= stickyTop){
+      stickyWrapper.height(stickyHeight);
+      sticky.addClass("is-sticky");
     }
-  }
-  
-  function makeNavbarDark() {
-    if (isLight && isTogglable) {
-      navbar.classList.remove('navbar-light');
-      navbar.classList.remove('bg-white');
-      navbar.classList.remove('border-bottom');
-      navbar.classList.add('navbar-dark');
-
-      repaintNav();
-
-      isLight = false;
+    else{
+      sticky.removeClass("is-sticky");
+      stickyWrapper.height('auto');
     }
-  }
-
-  // Repaint hack for Safari overscroll bug
-  function repaintNav() {
-    navbar.style.display='none';
-    navbar.offsetHeight;
-    navbar.style.display='block';
-  }
-
-  function toggleNavbar(event) {
-    var scrollTop = window.pageYOffset;
-
-    if (scrollTop > 0 && !isLight) {
-      makeNavbarLight();
-    } else if (scrollTop == 0 || scrollTop < 0 && isLight) {
-      makeNavbarDark();
-    }
-  }
-
+  };
   
-  //
-  // Events
-  //
-
-  if (navbar && isTogglable) {
-    "load scroll".split(' ').forEach(function(e) {
-      window.addEventListener(e, function(e) {
-        var type = e.type;
-
-        toggleNavbar(type);
-      });
+  // Find all data-toggle="sticky-onscroll" elements
+  $('[data-toggle="sticky-onscroll"]').each(function() {
+    var sticky = $(this);
+    var stickyWrapper = $('<div>').addClass('sticky-wrapper'); // insert hidden element to maintain actual top offset on page
+    sticky.before(stickyWrapper);
+    sticky.addClass('sticky');
+    
+    // Scroll & resize events
+    $(window).on('scroll.sticky-onscroll resize.sticky-onscroll', function() {
+      stickyToggle(sticky, stickyWrapper, $(this));
     });
-  }
-
-})();
+    
+    // On page load
+    stickyToggle(sticky, stickyWrapper, $(window));
+  });
+});
