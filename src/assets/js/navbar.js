76753f8f3ce2af50d8ed85c91f89513dc826f9f6
Jenn <jjalava456@hotmail.com>
2019-09-09 14:29:05 -0500

    mejorando el navbar, en chequealo.html se encuentra la transparencia

--- src/assets/js/navbar.js
+++ src/assets/js/navbar.js
@@ -1,79 +1,19 @@
-//
-// navbar.js
-// Theme module
-//
 
-'use strict';
+      $(document).ready(function(){
+        var scroll_start = 0;
+        var startchange = $('#change-it-now');
+        var offset = startchange.offset();
 
-(function() {
-  
-  //
-  // Variables
-  //
+        if(startchange.length){
+          $(document).scroll(function(){
+            scroll_start = $(this).scrollTop();
 
-  var navbar = document.querySelector('.navbar');
-  var isLight = false;
-  var isTogglable = navbar ? navbar.classList.contains('navbar-togglable') : false;
-
-
-  //
-  // Functions
-  //
-
-  function makeNavbarLight() {
-    if (!isLight && isTogglable) {
-      navbar.classList.remove('navbar-dark');
-      navbar.classList.add('navbar-light');
-      navbar.classList.add('bg-white');
-      navbar.classList.add('border-bottom');
-
-      isLight = true;
-    }
-  }
-  
-  function makeNavbarDark() {
-    if (isLight && isTogglable) {
-      navbar.classList.remove('navbar-light');
-      navbar.classList.remove('bg-white');
-      navbar.classList.remove('border-bottom');
-      navbar.classList.add('navbar-dark');
-
-      repaintNav();
-
-      isLight = false;
-    }
-  }
-
-  // Repaint hack for Safari overscroll bug
-  function repaintNav() {
-    navbar.style.display='none';
-    navbar.offsetHeight;
-    navbar.style.display='block';
-  }
-
-  function toggleNavbar(event) {
-    var scrollTop = window.pageYOffset;
-
-    if (scrollTop > 0 && !isLight) {
-      makeNavbarLight();
-    } else if (scrollTop == 0 || scrollTop < 50 && isLight) {
-      makeNavbarDark();
-    }
-  }
-
-  
-  //
-  // Events
-  //
-
-  if (navbar && isTogglable) {
-    "load scroll".split(' ').forEach(function(e) {
-      window.addEventListener(e, function(e) {
-        var type = e.type;
-
-        toggleNavbar(type);
-      });
-    });
-  }
-
-})();
+            if(scroll_start>offset.top){
+              $('.nav-kronio').css('background-color', 'rgba(0, 64, 123, 0.9)');
+            }else{
+              $('.nav-kronio').css('background-color', '#E55166');
+            }
+          })
+        }
+      })
+    
